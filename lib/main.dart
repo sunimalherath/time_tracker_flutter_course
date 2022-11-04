a120ad024008572837bfb061d98d108a5fd71f2b
sunimalherath <sunimaldv@gmail.com>
2022-11-04 20:11:38 +1100

    adding sign in page widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,11 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Time Tracker',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey
+        primarySwatch: Colors.blue
       ),
-      home: Container(
-        color: Colors.white,
-      ),
+      home: SignInPage(),
     );
   }
 }
