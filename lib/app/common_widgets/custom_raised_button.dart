f221ee0eeb120921e80202b194bb2603780baa63
sunimalherath <sunimaldv@gmail.com>
2022-11-17 23:12:18 +1100

    adding property to configure button height

--- lib/app/common_widgets/custom_raised_button.dart
+++ lib/app/common_widgets/custom_raised_button.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
 
 class CustomRaisedButton extends StatelessWidget {
-  const CustomRaisedButton({super.key, required this.child, required this.onPressed, required this.color});
+  const CustomRaisedButton({
+    super.key,
+    required this.child,
+    required this.onPressed,
+    required this.color,
+    this.height = 50.0,
+  });
 
   final Widget child;
   final VoidCallback? onPressed;
   final Color color;
+  final double height;
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: child,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: color
+    return SizedBox(
+      height: height,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: child,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color
+        ),
       ),
     );
   }
